dfaf16b16aee3c65061098dff1fa6eb163dca6cb
Micaela Iribarren <micaela_iribarren@hotmail.com>
2020-02-13 11:56:52 -0300

    Se crea pagina: to do details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'todo-details',
+    loadChildren: () => import('./pages/todo-details/todo-details.module').then( m => m.TodoDetailsPageModule)
+  },
 ];
 
 @NgModule({
