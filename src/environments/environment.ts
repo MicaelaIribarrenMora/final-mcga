4fe8885deb29986d73aa12517047f29c0cdcb7dd
Micaela Iribarren <micaela_iribarren@hotmail.com>
2020-02-13 12:05:03 -0300

    Se agrego Firebase y los datos al environmennt

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA9opSWqh0J6V22OSi2KowvsazsqKKxgQ8",
+    authDomain: "final-mcga.firebaseapp.com",
+    databaseURL: "https://final-mcga.firebaseio.com",
+    projectId: "final-mcga",
+    storageBucket: "final-mcga.appspot.com",
+    messagingSenderId: "697431338025",
+    appId: "1:697431338025:web:b9ee9a80b706d368a74038"
+  }
 };
 
 /*
