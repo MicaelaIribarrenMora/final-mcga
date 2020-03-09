5e16df4d89d77b5d3cac13759a0fc7b13e34dd88
Micaela Iribarren <micaela_iribarren@hotmail.com>
2020-03-09 20:34:46 -0300

    Se agrego la configuracion de firebase para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA9opSWqh0J6V22OSi2KowvsazsqKKxgQ8",
+    authDomain: "final-mcga.firebaseapp.com",
+    databaseURL: "https://final-mcga.firebaseio.com",
+    projectId: "final-mcga",
+    storageBucket: "final-mcga.appspot.com",
+    messagingSenderId: "697431338025",
+    appId: "1:697431338025:web:b9ee9a80b706d368a74038"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyA9opSWqh0J6V22OSi2KowvsazsqKKxgQ8",
     authDomain: "final-mcga.firebaseapp.com",
