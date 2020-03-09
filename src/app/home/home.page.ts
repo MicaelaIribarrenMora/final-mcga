a359bb90307691cd0f8614bf0baedcf3789321ee
unknown <emi_lvp23@hotmail.com>
2020-03-09 02:53:18 -0300

    home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo, TodoService } from '../services/todo.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +12,7 @@ import { Todo, TodoService } from '../services/todo.service';
 export class HomePage implements OnInit{
   todos: Todo[];
 
-  constructor(private todoService: TodoService) {}
+  constructor(private todoService: TodoService, private router: Router, private authSvc: AuthService, public afAuth: AngularFireAuth) {}
 
   ngOnInit() {
     this.todoService.getTodos().subscribe(res => {
@@ -19,6 +22,12 @@ export class HomePage implements OnInit{
 
   remove(item) {
     this.todoService.removeTodo(item.id);
+  }
+
+  onLogout(){
+    console.log('salir');
+    this.afAuth.auth.signOut();
+    this.router.navigateByUrl('/login');
   }
 
 }
